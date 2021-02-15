013b0dde46d431ee8777f855bdd1b096a4999928
Kasper H <ksh@awmnetwork.com>
2021-02-15 10:10:16 +0100

    update to match end of session 6

--- dnd/components/players/zerath.js
+++ dnd/components/players/zerath.js
@@ -4,23 +4,24 @@ class Zerath extends React.Component {
             <Player name="Zerath">
                 <Grouping name="Resources">
                     <TransitionResource name="Ammunition" transition="d12,d10,d8,d6,d4,1,0" /> 
-                    <ChargeResource name="Hit Die" charges="3" spend="1" />
+                    <ChargeResource name="Hit Die" charges="4"  />
                 </Grouping>
                 <Grouping name="Class abilities">
-                    <ChargeResource name="Bardic Inspiration" charges="3" spend="3" />
-                    <ChargeResource name="Fey Teleportation" charges="1" spend="1" />
+                    <ChargeResource name="Bardic Inspiration" charges="4" />
+                    <ChargeResource name="Fey Teleportation" charges="1" />
                 </Grouping>
                 <Grouping name="Spells">
-                    <ChargeResource name="1st level" charges="4" spend="2" />
-                    <ChargeResource name="2nd level" charges="2" spend="2" />
+                    <ChargeResource name="1st level" charges="4"  />
+                    <ChargeResource name="2nd level" charges="3"  />
                 </Grouping>
                 <Grouping name="Items">
                     <ChargeResource name="Ring of Rebellion" charges="1"/>
-                    <ChargeResource name="Vox Populi" charges="1" spend="1"/>
+                    <ChargeResource name="Vox Populi" charges="1" />
                     <TransitionResource name="Vox Populi Damage Type" transition="Piercing, Cold, Fire, Lightning, Acid, Necrotic"/> 
+                    <ChargeResource name="Box of Beasts" charges="1" />
+                    <ChargeResource name="A Very Nice Hat" charges="1" />
                 </Grouping>
                 <Grouping name="Consumeables">
-                    <QuantityResource name="Potion of Healing" quantity="1"/>
                     <QuantityResource name="Dragon's Breath Pepper" quantity="1"/>
                     <QuantityResource name="Serpent Venom" quantity="1"/>
                 </Grouping>
