e2666631b34484e9cdcf50d545cad55f467147f5
Kasper H <ksh@awmnetwork.com>
2021-03-22 12:26:56 +0100

    fix

--- dnd/components/app.js
+++ dnd/components/app.js
@@ -1,7 +1,7 @@
 class App extends React.Component {
     render() {
         return (
-            <div className="container">
+            <div className="container application">
                 {this.props.children}
             </div>
           
