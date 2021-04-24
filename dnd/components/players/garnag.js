800d7a3941fedbc210de43f036b72cf13a3bceb1
Kasper H <ksh@awmnetwork.com>
2021-04-25 01:28:58 +0200

    adjust post session 9

--- dnd/components/players/garnag.js
+++ dnd/components/players/garnag.js
@@ -3,8 +3,8 @@ class Garnag extends React.Component {
         return (
             <Player name="Garnag">
                 <Grouping name="Resources">
-                    <TransitionResource name="Ammunition" transition="d12,d10,d8,d6,d4,1,0" /> 
-                    <ChargeResource name="Hit Die" charges={4} onShortRest={(spend) => spend - 1 < 0 ? 0 : spend - 1} />
+                    <TransitionResource name="Ammunition" transition="d12,d10,d8,d6,d4,1,0" current="d10"/> 
+                    <ChargeResource name="Hit Die" charges={4} onShortRest={(spend) => spend - 1 < 0 ? 0 : spend - 1} spend={1}/>
                     <ChargeResource name="Inspiration" charges={1} onLongRest={(c) => c} /> 
                     <QuantityResource name="Gold" quantity={372} />
                 </Grouping>
@@ -19,7 +19,7 @@ class Garnag extends React.Component {
                 <Grouping name="Items">
                     <ChargeResource name="Yohn's Sword of Retribution" charges={4} />
                     <ChargeResource name="Cape of Hellish Resilience (Healing)" charges={1} onShortRest={(_) => 0} />
-                    <ChargeResource name="Cape of Hellish Resilience (Glare)" charges={1} />
+                    <ChargeResource name="Cape of Hellish Resilience (Glare)" charges={1} spend={1} />
                     <ChargeResource name="A Very Nice Hat" charges={1} />
                     <ChargeResource name="Ring of Spell Storing" charges={3} spend={3} onLongRest={(c) => c} /> 
                 </Grouping>
@@ -28,7 +28,7 @@ class Garnag extends React.Component {
                 </Grouping>
                 <Grouping name="Consumeables">
                     <QuantityResource name="Healing Potion" quantity="1"/>
-                    <QuantityResource name="Security Mega Drone Ray" quantity="2"/>
+                    <QuantityResource name="Security Mega Drone Ray" quantity="1"/>
                 </Grouping>
             </Player>
         )
--- dnd/components/players/zerath.js
+++ dnd/components/players/zerath.js
@@ -3,35 +3,33 @@ class Zerath extends React.Component {
         return (
             <Player name="Zerath">
                 <Grouping name="Resources">
-                    <TransitionResource name="Ammunition" transition="d12,d10,d8,d6,d4,1,0" /> 
-                    <ChargeResource name="Hit Die" charges={4} onShortRest={(spend) => spend - 1 < 0 ? 0 : spend - 1} />
+                    <TransitionResource name="Ammunition" transition="d12,d10,d8,d6,d4,1,0" current="d10"/> 
+                    <ChargeResource name="Hit Die" charges={4} spend={3} onShortRest={(spend) => spend - 1 < 0 ? 0 : spend - 1} />
                     <ChargeResource name="Inspiration" charges={1} onLongRest={(c) => c} /> 
                 </Grouping>
                 <Grouping name="Racial abilities">
                     <ChargeResource name="Fey Teleportation" charges={1} />
                 </Grouping>
                 <Grouping name="Class abilities">
-                    <ChargeResource name="Bardic Inspiration" charges={4} spend={1}/>
+                    <ChargeResource name="Bardic Inspiration" charges={4} spend={4}/>
                 </Grouping>
                 <Grouping name="Spells">
                     <ChargeResource name="1st level" charges={4}  />
-                    <ChargeResource name="2nd level" charges={3}  />
+                    <ChargeResource name="2nd level" charges={3} spend={3} />
                 </Grouping>
                 <Grouping name="Items">
                     <ChargeResource name="Ring of Rebellion" charges={2} />
                     <ChargeResource name="Box of Beasts" charges={1} />
                     <ChargeResource name="Cape of Hellish Resilience (Healing)" charges={1} onShortRest={(_) => 0} />     
                     <ChargeResource name="Vox Populi" charges={1} />
-                    <TransitionResource name="Vox Populi Damage Type" transition="Piercing, Cold, Fire, Lightning, Acid, Necrotic"/> 
+                    <TransitionResource name="Vox Populi Damage Type" transition="Piercing, Cold, Fire, Lightning, Acid, Necrotic" current="cold" /> 
                 </Grouping>
                 <Grouping name="Smithing">
                     <QuantityResource name="Pinning Shot" quantity="1"/>
                     <QuantityResource name="Perfectly Balanced" quantity="1"/>
                 </Grouping>
                 <Grouping name="Consumeables">
-                    <QuantityResource name="Dragon's Breath Pepper" quantity="1"/>
                     <QuantityResource name="Serpent Venom" quantity="1"/>
-                    <QuantityResource name="Security Mega Drone Ray" quantity="1"/>
                 </Grouping>
             </Player>
         )
